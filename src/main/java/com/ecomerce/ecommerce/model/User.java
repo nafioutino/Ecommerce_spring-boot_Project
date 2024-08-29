89d86349829a0e8ff2828051f164e9cdc135a040
Abdoul-Nafiou <nafiou531@gmail.com>
2024-08-29 11:36:30 +0100

    dbconfiguration

--- src/main/java/com/ecomerce/ecommerce/model/User.java
+++ src/main/java/com/ecomerce/ecommerce/model/User.java
@@ -2,17 +2,28 @@ package com.ecomerce.ecommerce.model;
 
 import java.time.LocalDateTime;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+// import lombok.EqualsAndHashCode;
+// import lombok.Getter;
+// import lombok.RequiredArgsConstructor;
+// import lombok.Setter;
+// import lombok.ToString;
+/* 
 @Getter
-@Setter
-@ToString
-@EqualsAndHashCode
-@RequiredArgsConstructor
+ @Setter
+ @ToString
+ @EqualsAndHashCode
+ @RequiredArgsConstructor
+*/
+
+@Data  // générer getter setter toString hashcode et constructeurs
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Long id;
     private String username;
     private String email;
